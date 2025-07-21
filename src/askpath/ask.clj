c1b9ee05202a2255230c842981a6dea57046cbe0
Daisuke Sakurai <d.sakurai0@gmail.com>
2025-07-21 17:01:13 +0900

    Get the snippets file from env var

--- src/askpath/ask.clj
+++ src/askpath/ask.clj
@@ -17,20 +17,25 @@
   ))
 
 (defn -main []
-  (let [snippets-json-str (slurp "snippets.json")
-        result (validate-schema
-                  (slurp "snippets-schema.json") ;; Might be slower than (io/input-stream "filename")
-                  snippets-json-str
-                 )
-        errors (:errors result)
-        snippets-map (json/parse-string snippets-json-str)
-        ]
-    (if (.isEmpty errors)
-      (println (get snippets-map "entries"))
-      ;; Print errors
-      (do
-        (println "JSON file does not follow the schema:")
-        (doseq [err errors]
-          (println (.getMessage err))))))
-  )
+  (let [snippets-file-env (System/getenv "ASK_SNIPPET_FILE")]
+    (let [snippets-file-path (if (nil? snippets-file-env) "snippets.json" snippets-file-env)
+          snippets-json-str (slurp snippets-file-path)
+          result (validate-schema
+                    (slurp "snippets-schema.json") ;; Might be slower than (io/input-stream "filename")
+                    snippets-json-str
+                   )
+          errors (:errors result)
+          snippets-map (json/parse-string snippets-json-str)
+          ]
+      (if (.isEmpty errors)
+        (do
+          (println (get snippets-map "entries"))
+          (println (System/getenv "ASK_SNIPPET_FILE"))
+          )
+        ;; Print errors
+        (do
+          (println "JSON file does not follow the schema:")
+          (doseq [err errors]
+            (println (.getMessage err))))))
+   ))
 
