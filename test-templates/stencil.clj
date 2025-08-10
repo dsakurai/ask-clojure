4ea715f6ffe51f2b554d7d83a2cde00bf876709b
Daisuke Sakurai <d.sakurai0@gmail.com>
2025-08-10 16:18:03 +0900

    Comment

--- test-templates/stencil.clj
+++ test-templates/stencil.clj
@@ -1,4 +1,6 @@
 #!/usr/bin/env -S clojure -Sdeps '{:deps {stencil/stencil {:mvn/version "0.5.0"}}}'
 
+;; Instead, use pogonos: https://github.com/athos/pogonos/tree/master
+
 (require '[stencil.core :refer [render-string]])
 (println (render-string "Hi there, {{name}}." {:name "Donald"}))
